dea21558e2c2406f6769ac3203e539eb8f0a76ea
chirag <chiragaparadh@gmail.com>
2021-03-15 09:51:08 +0530

    Created the numeric and operation buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,17 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   Widget calcbutton(String btntxt, Color btncolor, Color txtcolor) {
     return Container(
-        child: ElevatedButton(
-            onPressed: () {
-              //TODO: Add function for button pressed
-            },
-            child: Text(
-              btntxt,
-              style: TextStyle(color: txtcolor, fontSize: 35),
-            ),
-            style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0))))));
+      //TODO: change RaisedButton to ElevatedButton
+      child: RaisedButton(
+        onPressed: () {
+          //TODO: Add some functionality
+        },
+        child: Text(btntxt, style: TextStyle(color: txtcolor, fontSize: 35)),
+        shape: CircleBorder(),
+        color: btncolor,
+        padding: EdgeInsets.all(20),
+      ),
+    );
   }
 
   @override
@@ -61,7 +60,67 @@ class _CalculatorState extends State<Calculator> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 //button functions
-                calcbutton('AC', Colors.grey, Colors.black)
+                calcbutton('AC', Colors.grey, Colors.black),
+                calcbutton('+/-', Colors.grey, Colors.black),
+                calcbutton('%', Colors.grey, Colors.black),
+                calcbutton('/', Colors.amber[700], Colors.white),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //button functions
+                calcbutton('7', Colors.grey[850], Colors.white),
+                calcbutton('8', Colors.grey[850], Colors.white),
+                calcbutton('9', Colors.grey[850], Colors.white),
+                calcbutton('x', Colors.amber[700], Colors.white),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //button functions
+                calcbutton('4', Colors.grey[850], Colors.white),
+                calcbutton('5', Colors.grey[850], Colors.white),
+                calcbutton('6', Colors.grey[850], Colors.white),
+                calcbutton('-', Colors.amber[700], Colors.white),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //button functions
+                calcbutton('1', Colors.grey[850], Colors.white),
+                calcbutton('2', Colors.grey[850], Colors.white),
+                calcbutton('3', Colors.grey[850], Colors.white),
+                calcbutton('+', Colors.amber[700], Colors.white),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  padding: EdgeInsets.fromLTRB(34, 20, 128, 20),
+                  onPressed: () {},
+                  shape: StadiumBorder(),
+                  child: Text("0",
+                      style: TextStyle(fontSize: 35, color: Colors.white)),
+                  color: Colors.grey[850],
+                ),
+                calcbutton('.', Colors.grey[850], Colors.white),
+                calcbutton('=', Colors.amber[700], Colors.white),
               ],
             )
           ],
